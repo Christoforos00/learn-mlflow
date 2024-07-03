13bc1c4579839db36362867730c85854875059d7
Christoforos00 <christrakas7@gmail.com>
2024-07-03 14:49:51 +0200

    remove dagster

--- wine_example/dagster-wine-example/dagster_wine_example/repository.py
+++
@@ -1,8 +0,0 @@
-from dagster import load_assets_from_package_module, repository
-
-from dagster_wine_example import assets
-
-
-@repository
-def dagster_wine_example():
-    return [load_assets_from_package_module(assets)]
